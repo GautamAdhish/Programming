8acced7ce5e8c8dbe86d827cb46621353dcf1e4a
GautamAdhish <np01nt4a250076@islingtoncollege.edu.np>
2025-12-10 14:42:50 +0545

    Dec 10 Classes

--- Week6Tutorial/src/q1.java
+++ Week6Tutorial/src/q1.java
@@ -9,22 +9,24 @@ public class q1 {
         }
 
         int[] age = new int[5];
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 2; i++) {
             System.out.print("Enter the age: ");
             age[i] = sc.nextInt();
         }
-        for (int j = 0; j < 4; j++) {
-            System.out.println(age[j]);
+        for (int j = 0; j < 2; j++) {
+            System.out.print(age[j]+"\t");
         }
 
+        System.out.println();
         sc.nextLine();
-        String[] name = new String[5];
-        for (int i = 0; i < 4; i++) {
-            System.out.print("Enter the name of person "+ (i+1) +":");
-            name[i] = sc.nextLine();
+        String[] name = new String[2];
+        for (int l = 0; l < 2; l++) {
+            System.out.print("Enter the name of person "+ (l+1) +":");
+            name[l] = sc.nextLine();
+            System.out.println(l);
         }
-        for (int j = 0; j < 4; j++) {
-            System.out.println(name[j]);
+        for (String k : name) {
+            System.out.print(k+"\t");
         }
     }
 }
